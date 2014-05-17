03d74ed6d7766033d0dbb1de1a5a1dffaf032665
Irina Likhtina <iralight@gmail.com>
2014-05-17 15:05:00 -0400

    organizer

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Organizer::Application.config.secret_key_base = 'ef1c4679154f1ff181c4ee247b5a9060157680f140514ddfa4242ea1e8a9e9d7b3e9acbfef34d5da665e6b4c393770197ace4f1bbefbbe232f56e77e433b511e'
